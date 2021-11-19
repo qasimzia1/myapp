28e155f68e077009f968040a09b4a406af2737ce
qasimzia1 <qasimzia1>
2021-11-19 20:46:07 +0500

    new

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,29 @@ import { red } from '@mui/material/colors';
 import TextField from '@mui/material/TextField';
 
 function Home() {
-    const [firstname,setfirstname]=useState("qasim")
-    const [lastname,setlastname]=useState("khan")
+    const [firstname,setfirstname]=useState("qasim");
+    const [lastname,setlastname]=useState("khan");
+    const [persondata,setpersondata]=useState({
+        "fname":"",
+        "lname":"",
+    });
+
+    const changedata= (e)=> {
+        const {name,value}= e.target;
+        setpersondata((prevState)=> ({
+            ...prevState,
+            [name]:value,
+        }))
+        console.log(persondata)
+    }
+
+   const submitdata= ()=> {
+       const person ={
+           "fname":firstname,
+           "lname":lastname,
+       };
+   console.log(persondata)
+   }
     return (
         //<Container style={{backgroundColor:"red" }} maxWidth="sm"> for css
         <div style={{}}>
@@ -14,10 +35,12 @@ function Home() {
             <p>home page</p> <br />
             <TextField
           required
-          onChange={(e) =>setfirstname(e.target.value)}
+         //onChange={(e) =>setfirstname(e.target.value)}
+         onChange={(e) =>changedata(e)}
+          name="fname"
           id="outlined-required"
           label="qasim"
-          defaultValue={firstname}
+          Value={persondata.firstname}
         />
         <br />
         <p>
@@ -26,16 +49,17 @@ function Home() {
         
         <TextField
           required
-          onChange={(e) =>setlastname(e.target.value)}
+          onChange={(e) =>changedata(e)}
+          name="lname"
           id="outlined-required"
           label="last name"
-          defaultValue={lastname}
+          Value={persondata.lastname}
         /> <br />
         <p>
             {lastname}
         </p>
         <br/>
-            <Button variant="contained">Contained</Button>
+            <Button onClick={submitdata} variant="contained">submit data</Button>
             </Container>
         </div>
     )
